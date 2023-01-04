fee672b39b8f15434ae6e338db6e7d529974669d
Thalles Ryan <84140766+thallesryan@users.noreply.github.com>
2023-01-04 13:08:21 -0300

    Módulo de Categorias e suas rotas
    /*Carregamento dos modulos otimizados, o módulo só será carregado quando acessado por 'categories' */
    
    //categorias irá carregar as rotas de categorias... as rotas configuradas em CategoriesModule, por exemplo '/new', serão acessadas por 'categories/new'

--- finansys/src/app/app-routing.module.ts
+++ finansys/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesModule } from './pages/categories/categories.module';
 
-const routes: Routes = [];
+/*Carregamento dos modulos otimizados, o módulo só será carregado quando acessado por 'categories' */
+
+//categorias irá carregar as rotas de categorias... as rotas configuradas em CategoriesModule, por exemplo '/new', serão acessadas por 'categories/new'
+const routes: Routes = [
+  {path: 'categories', loadChildren: () => CategoriesModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
